ea4daa3f72e04b309185f929b56e5b5e5d4f4543
Iklon <pankraftak@gmail.com>
2018-10-26 20:44:28 +0200

    Add files via upload

---
+++ src/sample/Controller.java
@@ -0,0 +1,53 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.stage.FileChooser;
+import java.io.*;
+import java.util.List;
+
+public class Controller {
+    private FileReader reader;
+    private BufferedReader bufferedreader;
+    private List<Point> list;
+    private XYChart.Series series = new XYChart.Series();
+
+    @FXML
+    private LineChart<Number, Number> chart;
+
+    @FXML
+    private void load() {
+        FileChooser chooser = new FileChooser();
+        chooser.setInitialDirectory(new File("D:\\Users\\Iklon\\Desktop\\Graf"));
+        File file = chooser.showOpenDialog(null);
+        if (file != null) {
+            try {
+                reader = new FileReader(file);
+                bufferedreader = new BufferedReader(reader);
+                try{
+                    String line;
+                    String[] souradnice;
+                    while ((line = bufferedreader.readLine()) != null) {
+                        line = line.replace("context.lineTo(", "");
+                        line = line.replace(");", "");
+                        line = line.trim();
+                        souradnice = line.split(",");
+                        System.out.println("Souradnice x: " + Double.parseDouble(souradnice[0]) + " y: " + Double.parseDouble(souradnice[1]));
+                        list.add(new Point(Double.parseDouble(souradnice[0]), Double.parseDouble(souradnice[1])));
+                    }
+                }
+                catch (IOException ex) {ex.printStackTrace();}
+            } catch (FileNotFoundException exc) {exc.printStackTrace();}
+        }
+    }
+
+    @FXML
+    private void draw() {
+        series.getData().clear();
+        for(int k=0; k<list.size(); k++) {
+            series.getData().add(new XYChart.Data(list.get(k).getX(), list.get(k).getY()));
+        }
+        chart.getData().add(series);
+    }
+}
