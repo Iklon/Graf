219f7e1d6098b7fd9754819923d93f82ab60293b
Iklon <pankraftak@gmail.com>
2018-10-26 22:07:57 +0200

    responzivní

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.FileChooser;
 import java.io.*;
@@ -11,11 +12,11 @@ import java.util.List;
 public class Controller {
     private FileReader reader;
     private BufferedReader bufferedreader;
-    private List<Point> list = new ArrayList<>;
+    private List<Point> list = new ArrayList<>();
     private XYChart.Series series = new XYChart.Series();
 
     @FXML
-    private LineChart<Number, Number> chart;
+    private LineChart<Number, Number> chart = new LineChart<>(new NumberAxis(), new NumberAxis());
 
     @FXML
     private void load() {
@@ -34,8 +35,6 @@ public class Controller {
                         line = line.replace(");", "");
                         line = line.trim();
                         souradnice = line.split(",");
-                        System.out.println("Blbost");
-                        System.out.println("Souradnice x: " + Double.parseDouble(souradnice[0]) + " y: " + Double.parseDouble(souradnice[1]));
                         list.add(new Point(Double.parseDouble(souradnice[0]), Double.parseDouble(souradnice[1])));
                     }
                 }
@@ -46,9 +45,10 @@ public class Controller {
 
     @FXML
     private void draw() {
-        series.getData().clear();
+        if (series != null) series.getData().clear();
         for(int k=0; k<list.size(); k++) {
             series.getData().add(new XYChart.Data(list.get(k).getX(), list.get(k).getY()));
+            System.out.println("Cyklus:" + k + " X:" + list.get(k).getX() + " Y:" + list.get(k).getY());
         }
         chart.getData().add(series);
     }
